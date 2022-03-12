8296b409d2610bc6afdf2483eb5a96d960e318fa
Eziz <eziz_kurbannazarow@mail.ru>
2022-03-12 17:52:46 +0300

    start creating shapes

--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -2,4 +2,33 @@ import { Module } from '../core/module'
 
 export class ShapeModule extends Module {
 
+  constructor(type, text) {
+    super(type, text)
+
+    this.parentElem = document.querySelector('.container')
+
+    if (!this.parentElem) {
+      this.parentElem = document.createElement('div')
+      this.parentElem.classList.add('container')
+      document.body.append(this.parentElem)
+    }
+  }
+
+  // container() {
+  //   const parentElem = document.createElement('div')
+  //   parentElem.classList.add('container')
+  //   return parentElem
+  // }
+
+  triangle() {
+    const triangleElem = document.createElement('div')
+    triangleElem.classList.add('triangle')
+    this.parentElem.append(triangleElem)
+  }
+
+  trigger() {
+    this.triangle()
+  }
+
+
 }
